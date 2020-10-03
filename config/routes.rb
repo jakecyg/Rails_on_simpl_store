09035ff9d599a21377cf4112215b675b95d8021d
jakecyg <43617272+jakecyg@users.noreply.github.com>
2020-10-02 21:26:53 -0500

    implemented products view and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'product/index'
+  resources :products, only: %i[index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'product#index'
 end
