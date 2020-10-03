78e2382e675b90c656dcbd668932a64888581d70
jakecyg <43617272+jakecyg@users.noreply.github.com>
2020-10-02 21:45:11 -0500

    added foreign key reference

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 require 'csv'
 
-Product.delete_all
+# Product.delete_all
 
-676.times  do
-  new_prod = Product.find_or_create_by(title: Faker::Commerce.unique.product_name,
-                                       price: Faker::Commerce.price,
-                                       stock_quantity: Faker::Number.between(from: 1, to: 1000))
-  if(new_prod.valid?)
-    new_prod.save
-  end
-end
-puts "Created #{Product.count} new products."
+# 676.times  do
+#   new_prod = Product.find_or_create_by(title: Faker::Commerce.unique.product_name,
+#                                        price: Faker::Commerce.price,
+#                                        stock_quantity: Faker::Number.between(from: 1, to: 1000))
+#   if(new_prod.valid?)
+#     new_prod.save
+#   end
+# end
+# puts "Created #{Product.count} new products."
